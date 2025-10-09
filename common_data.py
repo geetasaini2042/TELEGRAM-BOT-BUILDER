82d444a1175a0c5257ccb1869fe34c5e5d4a04f0
geetasaini2042 <Geetasaini2042@gmail.com>
2025-10-09 12:01:59 +0530

    Update common_data.py

--- common_data.py
+++ common_data.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 PUBLIC_URL = os.getenv("PUBLIC_URL", "http://127.0.0.1:8000")
-
-BOTS_FILE
-BOT_DATA_FOLDER
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+BOTS_FILE = os.path.join(BASE_PATH, "bots.json") 
+BOT_DATA_FOLDER = os.path.join(BASE_PATH, "BOTS_DATA")
 os.makedirs(BOT_DATA_FOLDER, exist_ok=True)
